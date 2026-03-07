be404e03453d0cd84c27f77f6ab22d609b0bd75a
Kirk Saunders <torikirksaunders@gmail.com>
2026-03-06 22:12:57 -0500

    Got the event deligation for selecting an answer working. Now I need to get working on loading the page from the state fo the current round for the game

--- public/js/game.js
+++ public/js/game.js
@@ -1,2 +1,13 @@
 import { hostCodeLocalStorageKey } from "./globalVariables.js";
 
+document.getElementById("answer-grid").addEventListener("click", (e) => {
+    const regex = new RegExp("answer-.*");
+
+    if(regex.test(e.target.id))
+    {
+        const targetIdSplit = e.target.id.split('-');
+        const clickedAnswer = document.getElementById(`${targetIdSplit[0]}-${targetIdSplit[1]}`);
+        const answerNumber = parseInt(targetIdSplit[1], 10);
+        clickedAnswer.classList.toggle("hidden-answer");
+    }
+})
