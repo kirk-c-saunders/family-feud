ac15e95c452dfca6e91a4ded7640c00d4dcadcdf
Kirk Saunders <torikirksaunders@gmail.com>
2026-03-12 20:17:22 -0400

    removing template files created off the express crash course since I should have all the examples I need now.

--- controllers/genericControllerFile.js
+++
@@ -1,6 +0,0 @@
-/*
-    Format for creating an API Endpoint:
-        export const [methodName] = (req, res, next) => {[callback function definition]}
-    
-    EX: export const getPost = (req, res, next) => {...}
-*/
